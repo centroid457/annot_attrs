11c2074cce8dda6b9b4660ba012ef8ac41b1dcd6
admin <023>
2023-09-24 21:00:03 +0300

    add tests

--- _test.py
+++ _test.py
@@ -12,10 +12,10 @@ from annot_attrs import *
 # =====================================================================================================================
 class Test:
     # -----------------------------------------------------------------------------------------------------------------
-    def test__annots_set(self):
+    def test__annots_get_set(self):
         class Cls(AnnotAttrs):
             ATTR1: int
-            ATTR2: Optional[int] = None
+            ATTR2: int = 2
 
         assert Cls().annots_get_set() == {"ATTR1", }
 
@@ -31,6 +31,52 @@ class Test:
         inst.ATTR3 = 1
 
         assert Cls2().annots_get_set() == {"ATTR1", "ATTR3", }
+
+    def test__annots_get_dict(self):
+        class Cls(AnnotAttrs):
+            ATTR1: int
+            ATTR2: int = 2
+
+        obj = Cls()
+        try:
+            obj.annots_get_dict()
+        except Exx_AttrNotExist:
+            pass
+        else:
+            assert False
+
+        obj.ATTR1 = 1
+        assert obj.annots_get_dict() == {"ATTR1": 1}
+
+    def test__getattr(self):
+        class Cls(AnnotAttrs):
+            ATTR1: int
+            ATTR2: int = 2
+
+        assert Cls().ATTR2 == 2
+        assert Cls().attr2 == 2
+
+        try:
+            Cls().ATTR222
+        except Exx_AttrNotExist:
+            pass
+        else:
+            assert False
+
+    def test__getitem(self):
+        class Cls(AnnotAttrs):
+            ATTR1: int
+            ATTR2: int = 2
+
+        assert Cls()["ATTR2"] == 2
+        assert Cls()["attr2"] == 2
+
+        try:
+            Cls()["ATTR222"]
+        except Exx_AttrNotExist:
+            pass
+        else:
+            assert False
 
 
 # =====================================================================================================================

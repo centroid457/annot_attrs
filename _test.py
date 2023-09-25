91800e9a688fd4c20f7724e2d3ce1a45d7b72f26
admin <023>
2023-09-25 15:23:18 +0300

    test dataclass!

--- _test.py
+++ _test.py
@@ -5,6 +5,7 @@ import shutil
 from tempfile import TemporaryDirectory
 from typing import *
 from configparser import ConfigParser
+from dataclasses import dataclass
 
 from annot_attrs import *
 
@@ -78,6 +79,7 @@ class Test:
         else:
             assert False
 
+    # -----------------------------------------------------------------------------------------------------------------
     def test__obj(self):
         class Cls:
             ATTR1: int
@@ -89,7 +91,8 @@ class Test:
         assert AnnotAttrs().annots_get_set(obj) == {"ATTR1", }
         assert AnnotAttrs().annots_get_dict(obj) == {"ATTR1": 1}
 
-    def test__obj_and_NamedTuple(self):
+    # -----------------------------------------------------------------------------------------------------------------
+    def test__NamedTuple(self):
         try:
             class Cls(AnnotAttrs, NamedTuple):
                 ATTR1: int
@@ -100,6 +103,7 @@ class Test:
         else:
             assert True
 
+    def test__NamedTuple_by_obj(self):
         class Cls(NamedTuple):
             ATTR1: int
             ATTR2: int = 2
@@ -107,7 +111,66 @@ class Test:
         obj = Cls(1)
 
         assert AnnotAttrs().annots_get_set(obj) == {"ATTR1", }
-        assert AnnotAttrs().annots_get_dict(obj) == {"ATTR1": 1}
+        assert AnnotAttrs().annots_get_dict(obj) == {"ATTR1": 1, }
+
+    # -----------------------------------------------------------------------------------------------------------------
+    def test__dataclass(self):
+        @dataclass
+        class Cls(AnnotAttrs):
+            ATTR1: int
+            ATTR2: int = 2
+
+        assert Cls(1).annots_get_set() == {"ATTR1", }
+        assert Cls(1).annots_get_dict() == {"ATTR1": 1, }
+
+    def test__dataclass_by_obj(self):
+        # 10------------------
+        @dataclass
+        class Cls:
+            ATTR1: int
+            ATTR2: int = 2
+
+        class Cls2(Cls):
+            ATTR2: int = 22
+            ATTR3: int = 33
+
+        obj = Cls2(1)
+        assert AnnotAttrs().annots_get_set(obj) == {"ATTR1", }
+        assert AnnotAttrs().annots_get_dict(obj) == {"ATTR1": 1, }
+
+        # 01------------------
+        class Cls:
+            ATTR1: int
+            ATTR2: int = 2
+
+        @dataclass
+        class Cls2(Cls):
+            ATTR2: int = 22
+            ATTR3: int = 33
+
+        obj = Cls2(1)
+        assert AnnotAttrs().annots_get_set(obj) == {"ATTR1", }
+        try:
+            assert AnnotAttrs().annots_get_dict(obj) == {"ATTR1": 1, }
+        except Exx_AttrNotExist:
+            pass   # its GOOD!!!
+        else:
+            assert False
+
+        # 11------------------
+        @dataclass
+        class Cls:
+            ATTR1: int
+            ATTR2: int = 2
+
+        @dataclass
+        class Cls2(Cls):
+            ATTR2: int = 22
+            ATTR3: int = 33
+
+        obj = Cls2(1)
+        assert AnnotAttrs().annots_get_set(obj) == {"ATTR1", }
+        assert AnnotAttrs().annots_get_dict(obj) == {"ATTR1": 1, }
 
 
 # =====================================================================================================================

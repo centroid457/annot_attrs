abd9beac7574f28326989b1e7b328bc97d983b06
admin <023>
2023-10-04 00:15:02 +0300

    jast add DOCS

--- annot_attrs/main.py
+++ annot_attrs/main.py
@@ -3,25 +3,37 @@ from typing import *
 
 # =====================================================================================================================
 class Exx_AttrNotExist(Exception):
+    """Exception in case of not existed attribute in instance
+    """
     pass
 
 
 # =====================================================================================================================
 class AnnotAttrs:
-    """
+    """Check all annotated and not defined attributes in instance obtain values!
+
     DONT INHERIT with typing.NamedTuple! will raise!
-    For NamedTuple use obj-style! obj parameter!
+    For NamedTuple use as separated function with obj parameter!
     """
     def __getattr__(self, item: str) -> Union[str, NoReturn]:
+        """ability to access values by attribute with any letercase ClsInstance.key and *.KEY,
+        it is important in this project because
+        - from INI files python obtain names in lowercase
+        - from ENV - in uppercase
+        """
         if item in ["__isabstractmethod__", ]:
             return
         else:
             return self.attr_get_case_insensitive(item)
 
     def __getitem__(self, key: str) -> Union[str, NoReturn]:
+        """ability to access values by dict style ClsInstance["key"]
+        """
         return self.attr_get_case_insensitive(key)
 
     def __str__(self):
+        """just a pretty string.
+        """
         result = "AnnotAttrs:"
         data = self.annots_get_dict()
         if data:
@@ -32,11 +44,12 @@ class AnnotAttrs:
         return result
 
     def print(self) -> None:
+        """just a pretty print for debugging or research.
+        """
         print(self)
 
     def annots_get_set(self, obj: Optional[Any] = None) -> Set[str]:
-        """
-        :return: set of undefined annotated attributes in class (not instance!)
+        """get undefined annotated attributes in class (not instance!)
         """
         annots = set()
         if obj is None:
@@ -54,8 +67,7 @@ class AnnotAttrs:
         return annots
 
     def annots_get_dict(self, obj: Optional[Any] = None) -> Union[Dict[str, Any], NoReturn]:
-        """
-        :return: dict of undefined annotated attributes in class but defined in instance!
+        """get dict of undefined annotated attributes in class but defined in instance!
         """
         annots = dict()
         for name in self.annots_get_set(obj):
@@ -63,11 +75,12 @@ class AnnotAttrs:
         return annots
 
     def annots_get_values(self, obj: Optional[Any] = None) -> Union[Iterable[Any], NoReturn]:
+        """get iterable values of undefined annotated attributes in class but defined in instance!
+        """
         return self.annots_get_dict(obj).values()
 
     def attr_get_case_insensitive(self, name: str, obj: Optional[Any] = None) -> Union[str, NoReturn]:
-        """
-        get value for attr name without case sense.
+        """get value for attr name without case sense.
         if no attr name in source - raise!
         """
         if obj is None:
@@ -86,8 +99,7 @@ class AnnotAttrs:
             raise Exx_AttrNotExist(msg)
 
     def annots_check_values_exists(self, obj: Optional[Any] = None) -> Union[True, NoReturn]:
-        """
-        check all annotations have values!
+        """check all annotations have values!
         Raise if not any value
         """
         self.annots_get_dict(obj)

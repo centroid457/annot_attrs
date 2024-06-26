54dc4c4dfc1773762809ed8284a85073c3fd7759
Andrei Starichenko <centroid@mail.ru>
2024-06-26 09:41:20 +0300

    zero dicstrings

--- annot_attrs/keys_as_values.py
+++ annot_attrs/keys_as_values.py
@@ -48,22 +48,23 @@ class AnnotsClsKeysAsValues(metaclass=AnnotsClsKeysAsValues_Meta):
     used as simple string data container, same as OneWordStringsList with access to values by dot.
     ATTEMPT to get rid of bare data like list[str] ot tuple[str]!
 
-    WHY NOT ENUM?
-    -------------
-    0. enum is the next level but we need previous one.
-    1. sometimes Enum is OverComplicated just for simple string values.
-    2. usually enum use int value for name and you can change value and dont understand what it was before according for value=1
-    3. in enum you will get key/value name by unnecessary attributes like *.name/value
-    4. if we need only value and value is already equal to name so why we need value with name?
-    5. enum keep instances, not the final results.
-    6. in enum values are abstract (not valuable)! so why we need to use them if we dont care? and why we need to create not used extra data?
-    FINALLY: so here you can directly accessing to exact string values by class attributes.
+    BEST EXAMPLE (Designed specially for this case)
+    -----------------------------------------------
+    Suppose you have
+        States = ("ON", "OFF")
+    next you think it would be better using it over DOT access, so you create class
+        class States:
+            ON = "ON"
+            OFF = "OFF"
 
-    WHY NOT NAMEDTUPLE?
-    -------------------
-    1. its again!.. why we need duplicating values if we already have it in names?
-    all we want - access by '*.attribute' to the value,
-    so minimum we need is using annotations, that's enough for IDE checker and return string values!
+    just replace it for
+        class States(AnnotsClsKeysAsValues):
+            ON: str
+            OFF: str
+
+    ADVANTAGES
+    ----------
+    1. DRY
 
     USAGE
     -----
@@ -100,6 +101,23 @@ class AnnotsClsKeysAsValues(metaclass=AnnotsClsKeysAsValues_Meta):
 
         print(MyValues.VALUE1)      # "VALUE1"
         print(MyValues().VALUE1)    #AttributeError: 'MyValues' object has no attribute 'VALUE1'
+
+    WHY NOT ENUM?
+    -------------
+    0. enum is the next level but we need previous one.
+    1. sometimes Enum is OverComplicated just for simple string values.
+    2. usually enum use int value for name and you can change value and dont understand what it was before according for value=1
+    3. in enum you will get key/value name by unnecessary attributes like *.name/value
+    4. if we need only value and value is already equal to name so why we need value with name?
+    5. enum keep instances, not the final results.
+    6. in enum values are abstract (not valuable)! so why we need to use them if we dont care? and why we need to create not used extra data?
+    FINALLY: so here you can directly accessing to exact string values by class attributes.
+
+    WHY NOT NAMEDTUPLE?
+    -------------------
+    1. its again!.. why we need duplicating values if we already have it in names?
+    all we want - access by '*.attribute' to the value,
+    so minimum we need is using annotations, that's enough for IDE checker and return string values!
     """
     pass
 
